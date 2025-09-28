cb8e1f454c0e95a57d652deef12ea5fbfd45b733
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-09-28 16:54:00 +0300

    implement hamburger menu and mobile navbar.

--- website-projects/shortly/js/script.js
+++ website-projects/shortly/js/script.js
@@ -1,6 +1,18 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
 const input = document.getElementById("link-input");
 const linkForm = document.getElementById("link-form");
 const errMsg = document.getElementById("err-msg");
+
+btn.addEventListener("click", navToggle);
+
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
 
 const MSG = {
   empty: "Please enter something",
