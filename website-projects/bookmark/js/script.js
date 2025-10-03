43b4e7de533f3e9c4c6706882eedd8fb3d976156
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-10-03 21:10:30 +0300

    add FAQ section with accordion & smooth expand animation

--- website-projects/bookmark/js/script.js
+++ website-projects/bookmark/js/script.js
@@ -1,27 +1,20 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
 
-// Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
 
 function onTabClick(e) {
-  // Deactivate all tabs
-  tabs.forEach((tab) => {
-    tab.children[0].classList.remove(
-      "border-soft-red",
-      "border-b-4",
-      "md:border-b-0"
-    );
+  tabs.forEach((t) => {
+    const indicator = t.querySelector("[data-target]");
+    indicator.classList.remove("border-soft-red", "border-b-4");
   });
 
-  // Hide all panels
-  panels.forEach((panel) => panel.classList.add("hidden"));
+  panels.forEach((p) => p.classList.add("hidden"));
 
-  // Activate a new tab and panel based on the target
-  e.target.classList.add("border-soft-red", "border-b-4");
-  const classString = e.target.getAttribute("data-target");
-  document
-    .getElementById("panels")
-    .getElementsByClassName(classString)[0]
-    .classList.remove("hidden");
+  const tab = e.currentTarget;
+  const indicator = tab.querySelector("[data-target]");
+  indicator.classList.add("border-soft-red", "border-b-4");
+
+  const targetClass = indicator.getAttribute("data-target");
+  document.querySelector(`#panels .${targetClass}`).classList.remove("hidden");
 }
